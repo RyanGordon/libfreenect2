27ed1c21c5ce67fea3cc0e840ac8103974a575f2
Ryan Gordon <ryan@quizlet.com>
2016-01-19 09:06:29 -0800

    Debugging homebrew cmake deps

--- cmake_modules/FindLibUSB.cmake
+++ cmake_modules/FindLibUSB.cmake
@@ -7,33 +7,34 @@
 #  LibUSB_INCLUDE_DIR: the directory that contains the include file
 #  LibUSB_LIBRARIES:  the libraries
 
-IF(PKG_CONFIG_FOUND)
-  IF(DEPENDS_DIR) #Otherwise use System pkg-config path
-    SET(ENV{PKG_CONFIG_PATH} "${DEPENDS_DIR}/libusb/lib/pkgconfig")
-  ENDIF()
-  SET(MODULE "libusb-1.0")
-  IF(CMAKE_SYSTEM_NAME MATCHES "Linux")
-    SET(MODULE "libusb-1.0>=1.0.20")
-  ENDIF()
-  IF(LibUSB_FIND_REQUIRED)
-    SET(LibUSB_REQUIRED "REQUIRED")
-  ENDIF()
-  PKG_CHECK_MODULES(LibUSB ${LibUSB_REQUIRED} ${MODULE})
-
-  FIND_LIBRARY(LibUSB_LIBRARY
-    NAMES ${LibUSB_LIBRARIES}
-    HINTS ${LibUSB_LIBRARY_DIRS}
-  )
-  SET(LibUSB_LIBRARIES ${LibUSB_LIBRARY})
-
-  RETURN()
-ENDIF()
+#IF(PKG_CONFIG_FOUND)
+#  IF(DEPENDS_DIR) #Otherwise use System pkg-config path
+#    SET(ENV{PKG_CONFIG_PATH} "${DEPENDS_DIR}/libusb/lib/pkgconfig")
+#  ENDIF()
+#  SET(MODULE "libusb-1.0")
+#  IF(CMAKE_SYSTEM_NAME MATCHES "Linux")
+#    SET(MODULE "libusb-1.0>=1.0.20")
+#  ENDIF()
+#  IF(LibUSB_FIND_REQUIRED)
+#    SET(LibUSB_REQUIRED "REQUIRED")
+#  ENDIF()
+#  PKG_CHECK_MODULES(LibUSB ${LibUSB_REQUIRED} ${MODULE})
+#
+#  FIND_LIBRARY(LibUSB_LIBRARY
+#    NAMES ${LibUSB_LIBRARIES}
+#    HINTS ${LibUSB_LIBRARY_DIRS}
+#  )
+#  SET(LibUSB_LIBRARIES ${LibUSB_LIBRARY})
+#
+#  RETURN()
+#ENDIF()
 
 FIND_PATH(LibUSB_INCLUDE_DIRS
   NAMES libusb.h
   PATHS
     "${DEPENDS_DIR}/libusb"
     "${DEPENDS_DIR}/libusbx"
+    "/opt/libusb"
     ENV LibUSB_ROOT
   PATH_SUFFIXES
     include
@@ -46,6 +47,7 @@ FIND_LIBRARY(LibUSB_LIBRARIES
   PATHS
     "${DEPENDS_DIR}/libusb"
     "${DEPENDS_DIR}/libusbx"
+    "/opt/libusb"
     ENV LibUSB_ROOT
   PATH_SUFFIXES
     x64/Release/dll
